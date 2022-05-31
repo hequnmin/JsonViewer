300e0b934dd1a6b770157560fab11e525469bfca
Min Ho <hequnmin@gmail.com>
2022-05-31 16:11:32 +0800

    初始化.

---
+++ JsonViewer/PluginsManager.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+
+namespace EPocalipse.Json.Viewer
+{
+    class PluginsManager
+    {
+        List<IJsonViewerPlugin> plugins = new List<IJsonViewerPlugin>();
+        List<ICustomTextProvider> textVisualizers = new List<ICustomTextProvider>();
+        List<IJsonVisualizer> visualizers = new List<IJsonVisualizer>();
+        IJsonVisualizer _defaultVisualizer;
+
+        public PluginsManager()
+        {
+        }
+
+        public void Initialize()
+        {
+            InitDefaults();
+        }
+
+        private void InitDefaults()
+        {
+            if (this._defaultVisualizer == null)
+            {
+                AddPlugin(new JsonObjectVisualizer());
+                AddPlugin(new AjaxNetDateTime());
+                AddPlugin(new CustomDate());
+            }
+        }
+
+        private void AddPlugin(IJsonViewerPlugin plugin)
+        {
+            plugins.Add(plugin);
+            if (plugin is ICustomTextProvider)
+                textVisualizers.Add((ICustomTextProvider)plugin);
+            if (plugin is IJsonVisualizer)
+            {
+                if (_defaultVisualizer == null)
+                    _defaultVisualizer = (IJsonVisualizer)plugin;
+                visualizers.Add((IJsonVisualizer)plugin);
+            }
+        }
+
+        public IEnumerable<ICustomTextProvider> TextVisualizers
+        {
+            get
+            {
+                return textVisualizers;
+            }
+        }
+
+        public IEnumerable<IJsonVisualizer> Visualizers
+        {
+            get
+            {
+                return visualizers;
+            }
+        }
+
+        public IJsonVisualizer DefaultVisualizer
+        {
+            get
+            {
+                return _defaultVisualizer;
+            }
+        }
+    }
+}
